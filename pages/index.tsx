a8dbd87179b0f8eb671e8ad526a8834b49d97af8
Harpal Singh1 <harpalnitk1@gmail.com>
2023-02-09 16:13:19 +0530

    added loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
 import Link from 'next/link';
+import Loader from '../components/Loader';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -17,6 +16,8 @@ export default function Home() {
       }}>
          Jeff's Profile
       </Link>
+
+      <Loader show/>
     </div>
   )
 }
