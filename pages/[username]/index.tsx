e9535ce1dc6fda2e12642d203ff0c96c2281537a
Harpal Singh1 <harpalnitk1@gmail.com>
2023-02-10 14:46:53 +0530

    added profile page

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,9 +1,46 @@
+import UserProfile from '../../components/UserProfile';
+import PostFeed from '../../components/PostFeed';
+
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
 // static routes have poriority over dynamic routes 
 
-export default function UserProfilePage({}){
+// SSR is good strategy for this page
+//as it is a public page and will be generated on server
+//fresh for each new request
+
+//async function for SSR
+export async function getServerSideProps({query}){
+
+    const {username} = query;
+    const userDoc = await getUserWithUsername(username);
+
+    //JSON serializable data
+    let user = null;
+    let posts = null;
+if(userDoc){
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+    .collection('posts')
+    .where('published','==',true)
+    .orderBy('createdAt', 'desc')
+    .limit(5);
+//postToJSON needed because post has firestore timestamp 
+//and we need to convert it to number
+    posts = (await postsQuery.get()).docs.map(postToJSON)
+}
+
+    return{
+        props:{user,posts}, // will be passed to page component as props
+          }
+}
+
+
+
+export default function UserProfilePage({user,posts}){
     return(
         <main>
-            <h1>User Profile</h1>
+           <UserProfile user={user}/>
+           <PostFeed posts={posts}/>
         </main>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,36 @@ import styles from '@/styles/Home.module.css'
 import Link from 'next/link';
 import Loader from '../components/Loader';
 
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
+import { useState } from 'react';
+
+
+//Max posts to query per page
+
+const LIMIT= 1;
+//function for SSR
+export async function getServerSideProps(context){
+const postsQuery = firestore
+//collection group query ; queries any posts 
+//collection no matter where it is nested
+.collectionGroup('posts')
+.where('published', '==', true)
+.orderBy('createdAt', 'desc')
+.limit(LIMIT);
+const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+return {
+  props: { posts }, // will be passed to the page component as props
+};
+
+
+}
+
 
 export default function Home() {
   return (
-    <div>
-      <Link href={{
+    <main>
+      {/* <Link href={{
         pathname:'/[username]',
         query:{username:'jeffd23'}
        
@@ -17,7 +42,9 @@ export default function Home() {
          Jeff's Profile
       </Link>
 
-      <Loader show/>
-    </div>
+      <Loader show/> */}
+
+      {/* FOR HOMEPAGE WE WILL SHOW FIRST 10 POSTS WITH LOAD MORE BUTON  */}
+    </main>
   )
 }
